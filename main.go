6796d0b8c76ab5026791f1831dd6bf7d9d9522a7
Zahra N Daniar <zahra.daniar@Mohamads-MacBook-Air.local>
2019-08-01 21:40:41 +0700

    I added customerHandlerFunc and marketingOfficerHandler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
@@ -8,7 +9,7 @@ import (
 
 import "github.com/go-redis/redis"
 
-func checkUrl(w http.ResponseWriter, r *http.Request) {
+func customerHandler(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
 	shorturl := redisGet(message)
@@ -23,9 +24,64 @@ func checkUrl(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type request struct {
+	LongUrl  string `json: "longUrl"`
+	ShortUrl string `json: "shortUrl"`
+}
+
+type response struct {
+	StatusCode string `json: "statusCode"`
+	ShortUrl   string `json: "shortUrl"`
+	Timestamp  uint64 `json: "unixtimestamp"`
+}
+
+func marketingOfficerHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		var u request
+		if r.Body == nil {
+			http.Error(w, "Please send a request body", 400)
+			return
+		}
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		redisSet(u.ShortUrl, u.LongUrl)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "I can not do that.")
+	}
+}
 func main() {
-	http.HandleFunc("/", checkUrl)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+	finish := make(chan bool)
+
+	serverMarketingOfficer := http.NewServeMux()
+	serverMarketingOfficer.HandleFunc("/", marketingOfficerHandler)
+	serverCustomer := http.NewServeMux()
+	serverCustomer.HandleFunc("/", customerHandler)
+
+	go func() {
+		http.ListenAndServe(":8080", serverMarketingOfficer)
+	}()
+	go func() {
+		http.ListenAndServe(":80", serverCustomer)
+	}()
+	<-finish
+
+}
+
+func redisSet(shorturl string, longurl string) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := client.Set(shorturl, longurl, 0).Err()
+	if err != nil {
 		panic(err)
 	}
 }
